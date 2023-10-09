4d342a0e888c346b9fe920745c1b1e47c2066e77
MUKILTEO\1713357 <Kolbscash@gmail.com>
2023-10-09 08:49:51 -0700

    Player And Checkpoint Saving (hehe love player prefs)

---
+++ Assets/Kaycee/Checkpoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public string checkpointID;
+    public Transform Player;
+    public float XSave, YSave, ZSave;
+    //trigger this event upon interaction with objective, will save and load location of the player upon interaction with object
+    void SaveLoc()
+    {
+        XSave = Player.transform.position.x;
+        YSave = Player.transform.position.y;
+        ZSave = Player.transform.position.z;
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.SetInt(checkpointID, 1);
+        PlayerPrefs.SetFloat("X", XSave);
+        PlayerPrefs.SetFloat("Y", YSave);
+        PlayerPrefs.SetFloat("Z", ZSave);
+    }
+   
+}

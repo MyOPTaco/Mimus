91d53e2d734cda5e99848c626f52964ed128e7b7
MUKILTEO\1713357 <Kolbscash@gmail.com>
2023-10-17 10:19:51 -0700

    coordinate notes + Fuse Box Pos Work

---
+++ Assets/Kaycee/Coord_Notes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coord_Notes : MonoBehaviour
+{
+    //2.03 4.75 1.7
+//2.32 4.75 1.754
+
+//0.54Z for switches in comparison with holders while active
+//change .6 for changes in the y direction
+//change of .7 in Z direction for switch holders
+
+//Rotation for switches is 27
+}

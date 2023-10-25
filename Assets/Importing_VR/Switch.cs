b501fe089ce79d5090a0bf4184a6ee1f37a5023e
MUKILTEO\1713529 <1713529@mukilteo.wednet.edu>
2023-10-25 08:37:40 -0700

    making a switch

---
+++ Assets/Importing_VR/Switch.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip switchSound;
+
+    public bool on = false;
+    public bool switchHit = false;
+    public float switchRotation = 100;
+    private GameObject switchBase;
+
+    public Light spotLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        switchBase = transform.GetChild(0).gameObject;
+        spotLight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(switchHit == true)
+        {
+            source.PlayOneShot(switchSound);
+            switchHit = false;
+            on = !on;
+
+            if(on == true)
+            {
+                spotLight.enabled = true;
+                transform.rotation = Quaternion.Euler(transform.eulerAngles.x + switchRotation, transform.eulerAngles.y, transform.eulerAngles.z);
+            }
+            else
+            {
+                spotLight.enabled = false;
+                transform.rotation = Quaternion.Euler(transform.eulerAngles.x - switchRotation, transform.eulerAngles.y, transform.eulerAngles.z);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerHand"))
+        {
+            switchHit = true;
+        }
+    }
+}
